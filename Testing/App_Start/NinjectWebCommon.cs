9737f8bc04a08cb0f8f4a03b351431ae4c839ff1
Elena del Valle <elenadelvalematute@gmail.com>
2015-11-14 19:52:56 +0000

    Added a comment to know how to insert injection

--- Testing/App_Start/NinjectWebCommon.cs
+++ Testing/App_Start/NinjectWebCommon.cs
@@ -61,7 +61,10 @@ namespace Testing.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-
+            //#if DEBUG
+            //    kernel.Bind<IMailService>().To<MailService>().InRequestScope;
+            //#else
+            //#endif
         }        
     }
 }
